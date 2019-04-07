bb2a9db88e5cea2638d71a00dd23feb62a24ede1
Elisia Wright <wrighte@aldenv101.allegheny.edu>
2019-04-07 17:47:13 -0400

    fixed typo in file name

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,10 @@
 
 import os
 import sys
+
+GO_BACK_A_DIRECTORY = "/../"
+GO_INTO_DIRECTORY = "pytest_encourage"
+
+# set the system path to contain the previous directory
+MYPATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, MYPATH + GO_BACK_A_DIRECTORY + GO_INTO_DIRECTORY)

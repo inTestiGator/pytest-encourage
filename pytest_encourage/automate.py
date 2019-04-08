f64d712c2846d614397f13f758a30a55194f392d
Jahlia Finney <finneyj@aldenv107.allegheny.edu>
2019-04-08 16:45:30 -0400

    fixed function spacing

--- pytest_encourage/automate.py
+++ pytest_encourage/automate.py
@@ -2,8 +2,9 @@ import subprocess
 
 
 def getpylint_output():
-list = ["pipenv", "run", "pylint", "util", "-f", "json"]
-try:
-    automate = subprocess.check_output(list, stderr=subprocess.STDOUT)
-except subprocess.CalledProcessError as automate:
-    print (automate.output)
+    list = ["pipenv", "run", "pylint", "util", "-f", "json"]
+    try:
+        automate = subprocess.check_output(list, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as automate:
+        output = automate.output
+        print (automate.output)

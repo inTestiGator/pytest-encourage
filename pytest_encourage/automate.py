dbf09b6b788644b13a01a697ab611f279289a796
Jahlia Finney <finneyj@aldenv107.allegheny.edu>
2019-04-08 16:36:59 -0400

    added a function and imported from it

--- pytest_encourage/automate.py
+++ pytest_encourage/automate.py
@@ -1,5 +1,7 @@
 import subprocess
 
+
+def getpylint_output():
 list = ["pipenv", "run", "pylint", "util", "-f", "json"]
 try:
     automate = subprocess.check_output(list, stderr=subprocess.STDOUT)
--- pytest_encourage/util.py
+++ pytest_encourage/util.py
@@ -2,7 +2,7 @@
 import json
 from typing import List, Dict, Union
 
-
+from automate import getpylint_output
 def read_source_file(filepath: str) -> List[str]:
     """ Function which returns the lines of a file """
     # pylint: disable=invalid-name

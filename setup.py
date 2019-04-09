dc3e9241c75cdcbc47b1983adad089e5c6db7c98
Elisia Wright <wrighte@aldenv160.allegheny.edu>
2019-04-08 21:07:39 -0400

    fixed typo and added more content to setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import io
 import os
@@ -7,17 +8,27 @@ from setuptools import setup
 
 # Package meta-data
 NAME = 'pytest-encourage'
-DESCRIPTION = ''
+DESCRIPTION = 'A pytest plugin used for testing data coverage and providing '
+    + 'positive reinforcement.'
+LICENSE='GNU GPLv3'
 AUTHOR = 'Jahlia Finney, Aubrey Collins, Elisia Wright, Jared Scklenski'
-AUTHOR_EMAIL= 'finneyj@allegheny.edu, collinsa@allegheny.edu, wrighte@allegheny.edu, + scklenskij@allegheny.edu'
+AUTHOR_EMAIL= 'finneyj@allegheny.edu, collinsa@allegheny.edu, '
+    + 'wrighte@allegheny.edu, scklenskij@allegheny.edu'
 URL = 'https://github.com/inTestiGator/pytest-encourage'
 
 # Where it all begins
 setup(
     name=NAME,
     description=DESCRIPTION,
-    long_description=read('README.md'),
+    license=LICENSE,
+    long_description=read('README.rst'),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     url=URL,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Framework :: Pytest',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+    ]
 )

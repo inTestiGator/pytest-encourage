b19d58d002e46d64cf7b399f0302e8faa7c16127
Jared Szklenski <szklenskij@aldenv159.allegheny.edu>
2019-04-08 21:48:24 -0400

    updated function

--- pytest_encourage/hooks.py
+++ pytest_encourage/hooks.py
@@ -8,5 +8,13 @@ import subprocess
 import sys
 import tempfile
 
-def pytest_runtest_logstart((filename, linenum, testname)):
-    # run pylint using filename
+def pytest_runtest_logstart(nodeid, location):
+    # run pylint using filename, then pass to filter_assertions
+    """ signal the start of running a single test item.
+
+    This hook will be called **before** :func:`pytest_runtest_setup`, :func:`pytest_runtest_call` and
+    :func:`pytest_runtest_teardown` hooks.
+
+    :param str nodeid: full id of the item
+    :param location: a triple of ``(filename, linenum, testname)``
+    """

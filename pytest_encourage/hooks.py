4b78a2b98961bb36625d38fab682125ae20b2e29
Jared Szklenski <szklenskij@aldenv159.allegheny.edu>
2019-04-08 21:46:02 -0400

    updated imports

--- pytest_encourage/hooks.py
+++ pytest_encourage/hooks.py
@@ -1,6 +1,12 @@
 """Functions for discovering and executing hooks."""
 
-from _ import _
+import errno
+import io
+import logging
+import os
+import subprocess
+import sys
+import tempfile
 
 def pytest_runtest_logstart((filename, linenum, testname)):
     # run pylint using filename

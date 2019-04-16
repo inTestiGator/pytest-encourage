0e82112fcb2e3daf886e235d22a70b037a60fa9b
Aubrey Collins <collinsa@allegheny.edu>
2019-04-16 15:04:54 -0400

    Handle arbitrary comparators in is_double_negative check

--- pytest_encourage/checks.py
+++ pytest_encourage/checks.py
@@ -8,7 +8,10 @@ def is_double_negative(expr: ast.Compare) -> bool:
     # Note that Python comparisons can have more than two parts;
     # e.g. 1 < 2 != 3 is a valid expression which evaluates to True
     values = [expr.left] + expr.comparators
-    values = [v.value for v in values]  # Map ast.NameConstant to their values
+
+    # If each AST value is a wrapper for True / False / None, assign it to the
+    # value it's wrapping
+    values = [v.value if isinstance(v, ast.NameConstant) else v for v in values]
     operations = expr.ops
     for oper in reversed(operations):
         val = values.pop()

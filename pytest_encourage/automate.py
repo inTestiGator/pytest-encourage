ed127b4a5135d476ad27305992a5a87249ace7b0
Jahlia Finney <finneyj@allegheny.edu>
2019-04-16 16:03:30 -0400

    changed conftest

--- pytest_encourage/automate.py
+++ pytest_encourage/automate.py
@@ -1,15 +1,18 @@
 import subprocess
 import os.path
 from os import path
+import ast
+import inspect
 
 from pylint.checkers import base
+from checks import is_double_negative
 
-def runChecks():
-    try:
-    fh = open('/path/to/file', 'r')
-    # Store configuration file values
-        if fh is not none:
-
+def runChecks(item):
+    task = ast.parse(inspect.getsource(item.function))
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Assert):
+            if isinstance(node.test, ast.Compare):
+    checks.is_double_negative()
 
 def getpylint_output(path_to_file):
     list = ["pipenv", "run", "pylint", path_to_file, "-f", "json"]

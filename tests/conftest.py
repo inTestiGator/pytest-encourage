5acb688a64f4a474304eea3c00684cd1cbae76e3
“Chih-Jung <chenc@allegheny.edu>
2019-04-18 19:07:39 -0400

    tempfile

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,14 @@
 """Configuration file for the test suite"""
+import json
+import pytest
 
+
+@pytest.fixture(scope="session")
+def​ generate_json(tmpdir_factory):
+    """ make a temp directory in jason"""
+    a_dir = tmpdir_factory.mktemp('mydir')
+    a_file = a_dir.join('something.txt')
+    a_file.write('contents may settle during shipping')
+    another_file.write('something different')
 
 pytest_plugins = ["pytester"]

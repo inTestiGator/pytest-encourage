1fe52d7df1d77fc02b3bd8c515235946d3c1e854
“Chih-Jung <chenc@allegheny.edu>
2019-04-18 19:52:15 -0400

    warning message

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,21 @@
 """Configuration file for the test suite"""
+
+import os
+import sys
 import json
 import pytest
+import warnings
+
+
+if not sys.version_info >= (3, 5):
+warnings.simplefilter("error", category=DeprecationWarning)
+
+
+pytest_plugins = ["pytester"]
+
+
+MYPATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, MYPATH + "/../")
 
 
 @pytest.fixture(scope="session")
@@ -10,5 +25,3 @@ def​ generate_json(tmpdir_factory):
     a_file = a_dir.join('something.txt')
     a_file.write('contents may settle during shipping')
     another_file.write('something different')
-
-pytest_plugins = ["pytester"]

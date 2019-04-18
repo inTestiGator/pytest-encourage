a44713f28b4405dc97528fc359964bc5cf14103f
Jahlia Finney <finneyj@allegheny.edu>
2019-04-18 16:25:26 -0400

    tests for our checks

--- pytest_encourage/checks.py
+++ pytest_encourage/checks.py
@@ -98,3 +98,5 @@ def run_bool_op_checks(expr: ast.BoolOp, checks=BOOL_OP_CHECKS):
         if check(expr):
             failing.append(check.__doc__)
     return failing
+
+def is_len_checks(expr: ast.)

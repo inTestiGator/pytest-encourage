f8c0040e3a164dcd56ff8ae1db5136d800a4b000
Elisia Wright <wrighte@allegheny.edu>
2019-04-23 15:13:24 -0400

    fixed linting errors not connected to variables

--- tests/test_temp.py
+++ tests/test_temp.py
@@ -2,14 +2,17 @@
 from pytest_encourage import checks
 import pytest
 
+
 def test_temp():
     if True:
         assert True != False
 
+
 if __name__ == "__main__":
     powers = []
-    for i in range(1,10):
+    for i in range(1, 10):
         powers.append(2 ** i)
+
 
 def test_none():
     """Tests not none check"""
@@ -17,11 +20,13 @@ def test_none():
     checks.is_none_compare(left, oper, right)
     ast.parse("assert purse is not none")
 
+
 def test_comparechecks_fail():
-   """Tests check for failing Comparisons"""
-   cat = " "
-   dog = "a"
-   assert cat == dog
+    """Tests check for failing Comparisons"""
+    cat = " "
+    dog = "a"
+    assert cat == dog
+
 
 def test_too_many_and():
     """Tests check for too many ands"""
@@ -30,17 +35,21 @@ def test_too_many_and():
     tiger = "2"
     assert pig == cow & cow == tiger & pig == tiger
 
+
 def test_is_len_checks():
     """tests the len check"""
     book = ["A", "B", "C", "D"]
     assert len(book) == 4
 
+
 def test_bool_checks():
     """Tests boolean checks"""
+
 
 def test_false_checks():
     """Tests false checks"""
     assert False
+
 
 def test_true_checks():
     """Tests true checks"""

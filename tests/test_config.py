b3f7dca2ee301446cccd54ef7cc7fd1be800e08d
Aubrey Collins <aubreypc@users.noreply.github.com>
2019-05-01 17:35:49 -0400

    Tests for enabling and disabling checks

---
+++ tests/test_config.py
@@ -0,0 +1,39 @@
+""" Tests for using the config file to change plugin functionality """
+
+from pytest_encourage.checks import get_enabled_checks_from_config
+from pytest_encourage.checks import COMPARE_CHECKS, CONSTANT_CHECKS, BOOL_OP_CHECKS
+
+
+def test_config_file_enables_checks(temp_config_file_all_enabled):
+    """ Config file should allow checks to be enabled """
+    checks = get_enabled_checks_from_config(config_path=temp_config_file_all_enabled)
+    assert isinstance(checks, dict)
+    assert "COMPARE" in checks
+    assert "CONSTANT" in checks
+    assert "BOOL" in checks
+    assert checks["COMPARE"] == list(COMPARE_CHECKS)
+    assert checks["CONSTANT"] == list(CONSTANT_CHECKS)
+    assert checks["BOOL"] == list(BOOL_OP_CHECKS)
+
+
+def test_empty_config_file(temp_config_file_empty):
+    """ If the config file is empty, all checks should be enabled """
+    checks = get_enabled_checks_from_config(config_path=temp_config_file_all_enabled)
+    assert isinstance(checks, dict)
+    assert "COMPARE" in checks
+    assert "CONSTANT" in checks
+    assert "BOOL" in checks
+    assert checks["COMPARE"] == list(COMPARE_CHECKS)
+    assert checks["CONSTANT"] == list(CONSTANT_CHECKS)
+    assert checks["BOOL"] == list(BOOL_OP_CHECKS)
+
+
+def test_config_file_disables_checks(temp_config_file_all_disabled):
+    """ Config file should allow checks to be disabled """
+    checks = get_enabled_checks_from_config(config_path=temp_config_file_all_enabled)
+    assert "COMPARE" in checks
+    assert "CONSTANT" in checks
+    assert "BOOL" in checks
+    assert checks["COMPARE"] == []
+    assert checks["CONSTANT"] == []
+    assert checks["BOOL"] == []

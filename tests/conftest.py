e032d6d0f97a3b593a73b74e089f338b956e66be
Aubrey Collins <aubreypc@users.noreply.github.com>
2019-05-01 17:10:48 -0400

    Fixture for config file tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """Configuration file for the test suite"""
 import os
 import sys
+import pytest
 
 GO_BACK_A_DIRECTORY = "/../"
 GO_INTO_DIRECTORY = "pytest_encourage"
@@ -10,3 +11,23 @@ sys.path.insert(0, PREVIOUS_DIRECTORY + GO_BACK_A_DIRECTORY + GO_INTO_DIRECTORY)
 
 
 pytest_plugins = ["pytester"]
+
+
+@pytest.fixture
+def temp_config_file_all_enabled(tmpdir):
+    """ Fixture to generate a temporary config file and return its path """
+    config = tmpdir.join(".encouragerc")
+    config.write("""\
+[comparison checks]
+is_double_negative=true
+is_none_compare=true
+is_len_check=true
+
+[constant checks]
+is_true=true
+is_false=true
+
+[boolean operation checks]
+has_too_many_ands=true
+    """)
+    return str(config)

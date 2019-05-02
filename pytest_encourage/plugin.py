6a9e3ccf2e8022cf655b01e7d11d67252ec48a6e
Aubrey Collins <aubreypc@users.noreply.github.com>
2019-05-02 19:01:00 -0400

    Remove unused imports

--- pytest_encourage/plugin.py
+++ pytest_encourage/plugin.py
@@ -1,15 +1,5 @@
 """ Defines hook functions for Pytest """
-import sys
-from pytest_encourage.automate import getpylint_output
-from pytest_encourage.util import filter_assertions
 from pytest_encourage.checks import run_checks
-
-GO_BACK_A_DIRECTORY = "/../"
-GO_INTO_DIRECTORY = "pytest_encourage"
-
-# set the system path to contain the previous directory
-MYPATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, MYPATH + GO_BACK_A_DIRECTORY + GO_INTO_DIRECTORY)
 
 
 def pytest_runtest_call(item):

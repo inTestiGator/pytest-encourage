462e5ffe422c30fc81c03f3aeb862347c3ad45f3
Aubrey Collins <aubreypc@users.noreply.github.com>
2019-05-04 17:51:38 -0400

    Logging for when Pytest calls our hook

--- pytest_encourage/plugin.py
+++ pytest_encourage/plugin.py
@@ -4,6 +4,7 @@ from pytest_encourage.checks import run_checks
 
 def pytest_runtest_call(item):
     """ Runs linting checks on the function Pytest is currently testing """
+    print("Pytest called hook")
     failed = run_checks(item.function)
     for failed_check_msg in failed:
         print(failed_check_msg)

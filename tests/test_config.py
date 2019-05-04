1de9bf87f923be7e6c227fe5d7b1ad137760cfeb
Aubrey Collins <aubreypc@users.noreply.github.com>
2019-05-04 17:56:50 -0400

    Remove unused tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,27 +1,7 @@
 """ Tests for using the config file to change plugin functionality """
 
-from pytest_encourage.checks import get_enabled_checks_from_config, check_is_enabled
+from pytest_encourage.checks import get_enabled_checks_from_config
 from pytest_encourage.checks import COMPARE_CHECKS, CONSTANT_CHECKS, BOOL_OP_CHECKS
-
-
-def test_check_is_enabled_all(temp_config_file_all_enabled):
-    """ Looking up a check in the config file should return true if check
-        is enabled """
-    for check in COMPARE_CHECKS + CONSTANT_CHECKS + BOOL_OP_CHECKS:
-        assert check_is_enabled(check, config_path=temp_config_file_all_enabled)
-
-
-def test_check_is_enabled_none(temp_config_file_all_disabled):
-    """ Looking up a check in the config file should return false if check
-        is disabled """
-    for check in COMPARE_CHECKS + CONSTANT_CHECKS + BOOL_OP_CHECKS:
-        assert not check_is_enabled(check, config_path=temp_config_file_all_disabled)
-
-
-def test_check_is_enabled_empty(temp_config_file_empty):
-    """ If the config file is empty, all checks should be enabled """
-    for check in COMPARE_CHECKS + CONSTANT_CHECKS + BOOL_OP_CHECKS:
-        assert check_is_enabled(check, config_path=temp_config_file_empty)
 
 
 def test_config_file_enables_checks(temp_config_file_all_enabled):
